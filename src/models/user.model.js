7c8e3990fd0144b734240b8135f46397d3dee542
Jonathan-A-Herrera <00379823@uca.edu.sv>
2024-10-16 10:42:09 -0600

    :memo: Laboratorio 6

---
+++ src/models/user.model.js
@@ -0,0 +1,30 @@
+class User {
+    constructor(name, lastname, email) {
+    this.id = Date.now().toString();
+    this.name = name;
+    this.lastname = lastname;
+    this.email = email;
+    }
+    }
+
+    const users = [];
+const save = (user) => {
+users.push(user);
+
+return user;
+}
+module.exports = {
+User,
+save
+};
+
+const getAllUsers = () => {
+    return users;
+};
+
+module.exports = {
+    User,
+    save,
+    getAllUsers 
+};
+
